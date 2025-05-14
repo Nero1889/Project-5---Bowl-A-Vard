e795dfcd864c35ebd5bf7b39297ef7846d69d72b
Raul Luna <mceclipse1889@gmail.com>
2025-05-14 17:48:43 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import {StrictMode} from "react"
-import {createRoot} from "react-dom/client"
-import "./style.css"
-import "./utils.css"
-import "./header.css"
-import Header from "./Header.jsx"
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import "./style.css";
+import "./utils.css";
+import "./header.css";
+import "./hero.css"
+import Header from "./Header.jsx";
+import Hero from "./Hero.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Header/>
+        <Hero/>
     </StrictMode>
 )
 

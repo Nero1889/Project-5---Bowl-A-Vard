3220bb217f89e614af779a70aa135af3741229b9
Raul Luna <mceclipse1889@gmail.com>
2025-05-15 14:34:54 -0500

    Add files via upload

---
+++ src/Activities.jsx
@@ -0,0 +1,9 @@
+function Activities() {
+    return (
+        <div className="activities section">
+            <h1 id="bottomless-title">Bottomless Bowling Just $9.99!</h1>
+        </div>
+    )
+}
+
+export default Activities;

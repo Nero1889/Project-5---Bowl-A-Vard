6d72283a7fc694a7f39a035ec06ccf06e569222c
Raul Luna <mceclipse1889@gmail.com>
2025-05-16 21:37:52 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,24 @@ import {createRoot} from "react-dom/client";
 import "./style.css";
 import "./utils.css";
 import "./header.css";
-import "./hero.css"
-import "./activities.css"
+import "./hero.css";
+import "./deal.css";
+import "./team.css";
+import "./activities.css";
 import Header from "./Header.jsx";
 import Hero from "./Hero.jsx";
+import Deal from "./Deal.jsx";
+import Team from "./Team.jsx";
 import Activities from "./Activities.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Header/>
         <Hero/>
+        <Deal/>
+        <Team/>
         <Activities/>
+        <Hero/>
     </StrictMode>
 )
 

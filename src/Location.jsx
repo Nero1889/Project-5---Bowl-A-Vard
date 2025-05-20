904348fa19a1e629c8679f34bac9ed3c019388a9
Raul Luna <mceclipse1889@gmail.com>
2025-05-20 17:25:34 -0500

    Update Location.jsx

--- src/Location.jsx
+++ src/Location.jsx
@@ -20,11 +20,9 @@ function Location() {
             <iframe
                 id="map"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2903.783919988886!2d-89.3153175!3d43.0948999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8839e5910ff579f3%3A0x9df4c75c5859a87!2s2121+E+Springs+Dr%2C+Madison%2C+WI+53704!5e0!3m2!1sen!2sus!4v1715912145878!5m2!1sen!2sus"
-
             ></iframe>        
         </div>
     )
 }
 
 export default Location;
-
